24b50c608969b1f338ad5d1060ade4b7b5ada3c0
xXHachimanXx <globaldep90@gmail.com>
2020-02-15 09:32:34 -0200

    Modificando estilo do Header da página

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import Header from './components/header/Header';
+import "./styles.css"
 
+const App = () => (
+  <div className="App">
+    <Header />
+  </div>
+);
+/*
 function App() {
   return (
-    <div className="App">
-      <h1>Hello world</h1>
-    </div>
+    
   );
 }
-
+*/
 export default App;
