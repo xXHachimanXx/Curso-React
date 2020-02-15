65392c82ac5b67e2c3629760da5c9b66f445ee95
xXHachimanXx <globaldep90@gmail.com>
2020-02-15 17:43:00 -0200

    Mudando para um tema mais escuro

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import "./styles.css";
 
-const Header = () => ( <header id="main-header">Huntweb</header> );
+const Header = () => ( 
+    <header id="main-header">
+        <img id="left_img" src='./logo192.png'></img>
+        Huntweb
+        <img id="rigth_img" src='./logo192.png'></img>
+    </header> );
 
 export default Header;
